8715aebc5809f83481c3bfe204fe1db011b68e22
Surya CS <surya@Suryas-MacBook-Air.local>
2024-06-21 12:39:27 -0400

    Created test cases for number prettifer and added implementation of the class

---
+++ NumberPrettifier/Prettifier.cs
@@ -0,0 +1,66 @@
+﻿namespace NumberPrettifier
+{
+
+    public static class Prettifier
+    {
+        
+        public static string PrettifyNumber(double number)
+        {
+            //define thresholdfor million and quadrillion
+            const double millionThreshold = 1_000_000;
+            const double quadrillionThreshold = 1_000_000_000_000_000;
+
+            string suffix = "";
+            //assuming negative numbers should not be prettified
+            if (number < 0)
+            {
+                // return custom message
+                return "Negative Number";
+            }
+
+           //since the requirement is to prettify numbers greater than 6 digits
+            if (number < millionThreshold)
+            {
+                //return the given number as string if its less than a million
+                return number.ToString();
+            }
+
+            // assuming the requirement is to prettify numbers only utpo trillions
+            else if (number >= quadrillionThreshold)
+            {
+                //if the input is >=quadrillion
+                return "Number greater than Trillion";
+            }
+            //calculate the exponent based on log base 10
+            int exponent =(int) Math.Log10(number) / 3;  //dividing the exponent by 3 finds the magnitude of the number
+
+            switch (exponent)
+            {
+                //if the exponent is 2, then logarithm base 1000 of the number is between 6 and 9
+                case 2:
+                    suffix = "M";// return Million
+                    break;
+                //if the exponent is 3, then logarithm base 1000 of the number is between 9 and 12
+                case 3:
+                    suffix = "B"; //return Billion
+                    break;
+                //if the exponent is 4, then logarithm base 1000 of the number is between 12 and 15
+                case 4:
+                    suffix = "T"; //return Trillion
+                    break;
+                default:
+                    suffix = ""; // default suffix
+                    break;
+            }
+
+            // Scale the number down appropriately based on the exponent
+            double scaledNumber = number / Math.Pow(1000, exponent);
+            // Format the scaled number with one decimal place. It will also round off the deciaml to nearest digit
+            string prettifiedNumber = scaledNumber % 1 == 0 ? scaledNumber.ToString("0") : Math.Round(scaledNumber, 1).ToString("0.0");
+            //return the number which is prettifed
+            return prettifiedNumber + suffix;
+        }
+
+
+    }
+}
---
+++ TestNumberPrettifier/NumberPrettifierTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit;
+using NumberPrettifier;
+
+namespace TestNumberPrettifier;
+
+
+[TestFixture]
+public class PrettifierTests
+{
+    // Test cases for PrettifyNumber method
+
+    // Test case: Input 1000000 should return "1M"
+    [TestCase(1000000, ExpectedResult = "1M")]
+
+    // Test case: Input 2560000.34 should return "2.6M"
+    [TestCase(2560000.34, ExpectedResult = "2.6M")]
+
+    // Test case: Input 2500000.34 should return "2.5M"
+    [TestCase(2500000.34, ExpectedResult = "2.5M")]
+
+    // Test case: Input 532 should return "532"
+    [TestCase(532, ExpectedResult = "532")]
+
+    // Test case: Input 1123456789 should return "1.1B"
+    [TestCase(1123456789, ExpectedResult = "1.1B")]
+
+    // Test case: Input 1500 should return "1500"
+    [TestCase(1500, ExpectedResult = "1500")]
+
+    // Test case: Input 999999 should return "999999"
+    [TestCase(999999, ExpectedResult = "999999")]
+
+    // Test case: Input 1000000000000 should return "1T"
+    [TestCase(1000000000000, ExpectedResult = "1T")]
+
+    // Test case: Input 0 should return "0"
+    [TestCase(0, ExpectedResult = "0")]
+
+    // Test case: Input -1234567890 should return "Negative Number"
+    [TestCase(-1234567890, ExpectedResult = "Negative Number")]
+
+    // Test case: Input 987654321 should return "987.7M"
+    [TestCase(987654321, ExpectedResult = "987.7M")]
+
+    // Test case: Input 4500 should return "4500"
+    [TestCase(4500, ExpectedResult = "4500")]
+
+    // Large number in trillions
+    [TestCase(123456789012345, ExpectedResult = "123.5T")]
+
+    // Small decimal number
+    [TestCase(0.12345, ExpectedResult = "0.1")]
+
+    // Decimal number rounded to one decimal place
+    [TestCase(123.456, ExpectedResult = "123.5")] 
+    // Method under test: PrettifyNumber_ReturnsPrettifiedString
+    public string PrettifyNumber_ReturnsPrettifiedString(double number)
+    {
+        // Call Prettifier.PrettifyNumber method and return the result
+        return Prettifier.PrettifyNumber(number);
+    }
+}
